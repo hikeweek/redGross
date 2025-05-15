dc45b4ad3d0381b8e74d8493d2d359545651144b
hikeweek <fueguchi.mail@gmail.com>
2025-05-15 19:06:57 +0300

    first prototype

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour {
+
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInputActions _playerInputActions;
+
+    public event EventHandler _OnPlayerAttack;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Enable();
+
+        _playerInputActions.Combat.Attack.started += PlayerAttack_started;
+    }
+
+    public Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        return inputVector;
+    }
+
+    public Vector3 GetMousePositon()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
+    }
+
+    public void DisableMovement()
+    {
+        _playerInputActions.Disable();
+    }
+
+    private void PlayerAttack_started(InputAction.CallbackContext obj)
+    {
+        _OnPlayerAttack?.Invoke(this, EventArgs.Empty);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour {
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private const string IS_RUNNING = "IsRunning";
+    private const string IS_DIE = "IsDie";
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        Player.Instance.OnPlayerDeath += Player_OnPlayerDeath;
+    }
+
+    private void Player_OnPlayerDeath(object sender, System.EventArgs e)
+    {
+        animator.SetBool(IS_DIE, true);
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_RUNNING, Player.Instance.IsRunning());
+
+        if (Player.Instance.IsAlive())
+            AdjustPlayerFacingDirection();
+
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = GameInput.Instance.GetMousePositon();
+        Vector3 playerPosition = Player.Instance.GetPlayerScreenPosition();
+
+        if (mousePos.x < playerPosition.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+}
